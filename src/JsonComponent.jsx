71bc20686cff9d3fee18b511a133d39c72461c57
Maria Kim <kim@projektionisten.de>
2019-05-27 17:20:25 +0200

    mxgraph laden in JsonComponent nach ComponentDidMount

--- src/JsonComponent.jsx
+++ src/JsonComponent.jsx
@@ -12,6 +12,27 @@ class JsonComponent extends React.Component{
         });
     }
 
+    componentDidMount=()=>{
+        var container = ReactDOM.findDOMNode(this.refs.mxgraphDiv);
+        if (!mxClient.isBrowserSupported()) {
+            // Displays an error message if the browser is not supported.
+            mxUtils.error("Browser is not supported!", 200, false);
+          } else {
+            const graph = new mxGraph(container);
+            var parent = graph.getDefaultParent();
+            graph.getModel().beginUpdate();
+             try{
+                var v1 = graph.insertVertex(parent, null, 'Hello,', 20, 20, 80, 30);
+                var v2 = graph.insertVertex(parent, null, 'World!', 200, 150, 80, 30);
+                var e1 = graph.insertEdge(parent, null, '', v1, v2);
+        
+             }
+             finally{
+                graph.getModel().endUpdate();
+             }
+          }
+    }
+
     render(){
         XtextServices.addSuccessListener((serviceType, result) => {
             if (serviceType === 'json') {
