bdfe040f07237ab7f38993f46e380c25e54b5e8c
Maria Kim <kim@projektionisten.de>
2019-06-03 13:28:21 +0200

    LOGIC ERROR: -if mxgraph-object is created in constructor, and the execute-method is ran on mxgraphmodel in ComponentDidMount, there is an error message "mxgraph-js.js:8089 Uncaught TypeError: Cannot read property 'execute' of undefined"

--- src/JsonComponent.jsx
+++ src/JsonComponent.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import "./JsonComponent.css"
 import XtextServices from "./ServerConnection/xtextServices";
 import {mxClient, mxGraph, mxUtils} from "mxgraph-js"
 class JsonComponent extends React.Component{
     constructor() {
         super()
+        var container = ReactDOM.findDOMNode(this.refs.mxgraphDiv);
+        const graph = new mxGraph(container);
+        const graph2 = new mxGraph(container);
+        this.state={
+            graph:graph,
+            clone:graph2
+        }
         XtextServices.addSuccessListener((serviceType, result) => {
             if (serviceType === 'json') {
-
+                const {graph}=this.state;
+                let parent=graph.getDefaultParent();
+                var v1 = graph.insertVertex(parent, null, 'JSON', 50, 50, 100, 100);
+                //this.setState({graph:graph});
             }
         });
     }
@@ -18,29 +29,42 @@ class JsonComponent extends React.Component{
             // Displays an error message if the browser is not supported.
             mxUtils.error("Browser is not supported!", 200, false);
           } else {
-            const graph = new mxGraph(container);
+            const graph_clone=Object.assign(this.state.graph);
+            const {graph} = this.state;
+            //const graph = new mxGraph(container);
             var parent = graph.getDefaultParent();
             graph.getModel().beginUpdate();
              try{
+                console.log(graph.getModel().updateLevel)
                 var v1 = graph.insertVertex(parent, null, 'Hello,', 20, 20, 80, 30);
                 var v2 = graph.insertVertex(parent, null, 'World!', 200, 150, 80, 30);
                 var e1 = graph.insertEdge(parent, null, '', v1, v2);
         
              }
              finally{
+                graph.getModel().execute();
                 graph.getModel().endUpdate();
+                console.log(graph.getModel().updateLevel)
              }
+             this.setState({graph})
+             // this.setState({clone:graph_clone})
+             console.log(this.state)
+             console.log(graph_clone)
           }
     }
 
     render(){
+        const {graph, clone}=this.state;
         XtextServices.addSuccessListener((serviceType, result) => {
             if (serviceType === 'json') {
                 console.log("render");
+                console.log(graph);
+                console.log("clone")
+                console.log(clone);
             }
         });
         return (
-            <div className="container" ref="mxgraphDiv"> </div>
+            <div className="container" ref="mxgraphDiv" > </div>
         );
     }
 }
