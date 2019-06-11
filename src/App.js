ee42aa29a2749dfa4177c8cef44f22edfad6eea4
Maria Kim <kim@projektionisten.de>
2019-06-11 13:56:54 +0200

    xtextservices aus app.js entfernen (werden erstmal nicht benötigt)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import './App.css';
-import XtextServices from "./ServerConnection/xtextServices";
 
 import GraphComponent from './GraphComponent'
 
 class App extends React.Component {
-
-    constructor() {
-        super()
-
-        XtextServices.addSuccessListener((serviceType, result) => {
-            if (serviceType === 'json') {
-
-            }
-        });
-
-    }
-
     render() {
         return (
             <div className="App">
