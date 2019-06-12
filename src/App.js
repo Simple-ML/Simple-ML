32481275b311eac417ccb8d4fc8f534acd02ed93
Maria Kim <kim@projektionisten.de>
2019-06-12 13:26:17 +0200

    Background + npm sass importieren

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 import GraphComponent from './GraphComponent'
 
@@ -7,7 +7,11 @@ class App extends React.Component {
     render() {
         return (
             <div className="App">
-                <GraphComponent />
+                <div className="app-header"></div>
+                <div className="ide-container">
+                <GraphComponent name="graph-container"/>
+                </div>
+                
             </div>
         );
     }
