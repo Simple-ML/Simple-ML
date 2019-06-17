20ff449c3c1cd5e5d2ecbf6bf566d8a76af118b2
Maria Kim <kim@projektionisten.de>
2019-06-17 15:27:10 +0200

    formatierung editorheader

--- src/Components/editorView/EditorHeader/EditorHeader.jsx
+++ src/Components/editorView/EditorHeader/EditorHeader.jsx
@@ -6,20 +6,18 @@ import "./EditorHeader.scss"
 class EditorHeader extends React.Component{
     constructor(props){
         super(props)
-        this.state={
-            checked:true,
+        this.state = {
+            checked: true,
         }
     }
     
     render() {
         return(
-            <div className="app-header">
-             <img src={logo} alt={"logo"} className="logo"></img>
+            <div className = "app-header">
+                <img src = { logo } alt = { "logo" } className= "logo"></img>
             </div>
         )
     }
-
 }
-
 
 export default EditorHeader;
