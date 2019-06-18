846d5ba774bb85cc883f46b95fc76ae90f8e5861
Lars Reimann <lars-reimann@outlook.com>
2019-06-18 16:23:13 +0200

    Add initial support for decision trees and random forests

--- lib/ml/scikitAdapter.py
+++ lib/ml/scikitAdapter.py
@@ -1,4 +1,17 @@
 from sklearn import linear_model
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+
+
+def decision_tree_regressor(x, y, config=None):
+    if config is None:
+        config = {}
+    config.setdefault("max_leaf_nodes", 100)
+    return DecisionTreeRegressor(max_leaf_nodes=config["max_leaf_nodes"]).fit(x, y)
+
+
+def random_forest_regressor(x, y):
+    return RandomForestRegressor().fit(x, y)
 
 
 def regression(x, y, config=None):
--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
 
+# TODO rename project (lib is too generic)
 setup(
     name='lib',
     version='0.0.1',
     description='Adapters for various ML libraries',
-    packages=['lib']
+    packages=['lib'], install_requires=['pandas', 'scikit-learn']
 )
