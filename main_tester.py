851d46b69a7161d997d12e4a148fdf552f7856ef
Lars Reimann <mail@larsreimann.com>
2019-07-09 13:53:42 +0200

    Change package and project names of adapters

--- main_tester.py
+++
@@ -1,3 +0,0 @@
-from lib.ml import PytorchAdapter as ptorch
-NN= ptorch.NN()
-loss_function
--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 from setuptools import setup
 
-# TODO rename project (lib is too generic)
 setup(
-    name='lib',
+    name='Simple-ML',
     version='0.0.1',
     description='Adapters for various ML libraries',
-    packages=['lib'], install_requires=['pandas', 'scikit-learn']
+    packages=['simpleml'], install_requires=['pandas', 'scikit-learn']
 )
