529854e638f40763780e1f081c6be9562d9868d5
Albert Danewitz <danewitz@projektionisten.de>
2019-08-27 10:18:16 +0200

    removed react-redux provider from index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import store from './reduxStore';
 
 import './index.css';
@@ -32,9 +31,7 @@ window.loadEditor((xtextEditor) => {
     });
 
     ReactDOM.render(
-        <Provider store={store}>
-            <App/>
-        </Provider>,
+        <App/>,
         document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
