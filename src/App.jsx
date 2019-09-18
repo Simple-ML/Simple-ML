d6a74fbc0b39a97ae1dfa845796b72807cd765d0
Maria Kim <kim@projektionisten.de>
2019-09-18 15:38:44 +0200

    no message

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ class App extends React.Component {
     render() {
         return (
             <div className="App">
-                <ChooseProjectView/>
+                <EditorView/>
 
                 <Toolbar/>
             </div>
