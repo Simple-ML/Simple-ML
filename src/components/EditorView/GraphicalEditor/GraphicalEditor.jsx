c401d160c1d8c1ee9db55a111a2aa0d6e9424e57
Maria Kim <kim@projektionisten.de>
2019-11-17 15:09:43 +0100

    icons eingebaut und kleine style anpassungen

--- src/components/EditorView/GraphicalEditor/GraphicalEditor.jsx
+++ src/components/EditorView/GraphicalEditor/GraphicalEditor.jsx
@@ -9,7 +9,7 @@ import SMLGraph from "./SMLGraph"
 //styles
 import background from './../../../styles/background.module.scss'
 import graphicalEditorStyle from './graphicalEditor.module.scss';
-import createButtonIcon from '../../../images/graph/instances/plus.svg';
+import createButtonIcon from '../../../images/graph/instances_mvp/flow-plus-secondary.svg';
 //redux
 import './toolbar.inference';
 import './createButton.inference';
