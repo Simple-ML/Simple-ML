3b5099c9f462ecdc9b911430777afecf72fda7c3
Lars Reimann <mail@larsreimann.com>
2020-12-01 15:15:32 +0100

    Change package structure of Python files

--- python/simpleml/ml/scikitAdapter.py
+++
@@ -1,9 +0,0 @@
-from python.simpleml.l3s.api import Query
-
-
-def fit(model, x: Query, y: Query):
-    return model.fit(x.getData(), y.getData())
-
-
-def predict(model, x):
-    return model.predict(x)
