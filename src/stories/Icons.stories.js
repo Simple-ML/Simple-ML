a832d228d942116d951d49845a63b7bfe0ff9028
Anzumana <anzumana.sander@gmail.com>
2020-12-04 07:18:28 +0100

    feat: initial stories storybook

---
+++ src/stories/Icons.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import  Icons from './Icons'
+export default {
+  title: 'SimpleML/Icons & Components',
+  component: Icons,
+};
+
+const Template = (args) => <Icons {...args} />;
+
+export const Icon = Template.bind({});
