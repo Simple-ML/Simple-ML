e631e9383e59ce87c0385259583fe59e9996e65b
Anzumana <anzumana.sander@gmail.com>
2020-12-11 08:35:15 +0100

    docs: updated storybook compponents

--- src/stories/Icons.stories.js
+++ src/stories/Icons.stories.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import  Icons from './Icons'
+import { withDesign } from 'storybook-addon-designs'
 export default {
-  title: 'Icons',
+  title: 'Components/Icon',
   component: Icons,
+	decorators: [withDesign],
+	parameters: {
+			backgrounds: {
+				default: 'violet',
+				values: [
+					{ name: 'dark', value: '#461f4c' },
+					{ name: 'light', value: 'white' },
+				],
+			},
+		design: {
+			type: 'figma',
+			url:
+				'https://www.figma.com/file/QMvF0qb7diOxDsPYIfvjSg/Design-MVP-2.0?node-id=8%3A6',
+		}
+	}
 };
 const Template = (args) => <Icons {...args} />;
 
