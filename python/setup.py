9130053234cf1506774dec3d3af975468b6d850f
fakharabbas-3002 <fakhar_s124@yahoo.com>
2021-03-15 17:13:06 +0100

    Move simpleml folder to python folder

--- python/setup.py
+++ python/setup.py
@@ -4,5 +4,5 @@ setup(
     name='Simple-ML',
     version='0.0.1',
     description='Adapters for various ML libraries',
-    packages=['simpleml'], install_requires=['pandas', 'scikit-learn', 'torch', 'pyspark', 'requests','json','shapely']
+    packages=['simpleml'], install_requires=['pandas', 'scikit-learn', 'torch', 'pyspark', 'requests','shapely']
 )
