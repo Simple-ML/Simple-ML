d1379e40596bac1c1627d8bef19c0528fc5270e1
Simon Gottschalk <gottschalk@l3s.de>
2021-03-26 12:16:11 +0100

    Rounded numbers for JSON statistics

--- python/simpleml/util/_export_util.py
+++ python/simpleml/util/_export_util.py
@@ -1,10 +1,19 @@
 import json
 from pandas import DataFrame
+from json import encoder
 
 
 def exportAsJSON(df: DataFrame):
     return df.to_json()
 
 
-def exportDictionaryAsJSON(dictionary: dict):
-    return json.dumps(dictionary)
+def exportDictionaryAsJSON(dictionary: dict, precision=2):
+    return json.dumps(round_floats(dictionary, precision))
+
+
+def round_floats(o, precision):
+    # Source: https://stackoverflow.com/a/53798633
+    if isinstance(o, float): return round(o, precision)
+    if isinstance(o, dict): return {k: round_floats(v, precision) for k, v in o.items()}
+    if isinstance(o, (list, tuple)): return [round_floats(x, precision) for x in o]
+    return o
