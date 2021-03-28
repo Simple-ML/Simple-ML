dba054a62db0692b1672197d0a2751aae74326aa
Simon Gottschalk <gottschalk@l3s.de>
2021-03-29 01:47:06 +0200

    transform temporal values in output

--- python/simpleml/util/_export_util.py
+++ python/simpleml/util/_export_util.py
@@ -1,6 +1,9 @@
 import json
 from pandas import DataFrame
 from json import encoder
+from numpy import datetime64
+import pandas as pd
+import simpleml.util._jsonLabels_util as config
 
 
 def exportAsJSON(df: DataFrame):
@@ -8,12 +11,21 @@ def exportAsJSON(df: DataFrame):
 
 
 def exportDictionaryAsJSON(dictionary: dict, precision=2):
-    return json.dumps(round_floats(dictionary, precision))
+    return json.dumps(round_floats_and_transform_temporal(dictionary, precision))
 
 
-def round_floats(o, precision):
+def round_floats_and_transform_temporal(o, precision):
     # Source: https://stackoverflow.com/a/53798633
+
+    if isinstance(o, (datetime64)):
+        ts = pd.to_datetime(str(o))
+        if config.lang == 'de':
+            d = ts.strftime('%d.%m.%Y, %H:%M:%S')
+        else:
+            d = ts.strftime('%Y-%m-%d %H:%M:%S')
+        return d
+
     if isinstance(o, float): return round(o, precision)
-    if isinstance(o, dict): return {k: round_floats(v, precision) for k, v in o.items()}
-    if isinstance(o, (list, tuple)): return [round_floats(x, precision) for x in o]
+    if isinstance(o, dict): return {k: round_floats_and_transform_temporal(v, precision) for k, v in o.items()}
+    if isinstance(o, (list, tuple)): return [round_floats_and_transform_temporal(x, precision) for x in o]
     return o
