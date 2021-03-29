9719461c27bbe81c449791a99880d8fe70561585
Simon Gottschalk <gottschalk@l3s.de>
2021-03-29 03:02:52 +0200

    update winebasic example and stub parameter names

--- tests/winebasic.py
+++ tests/winebasic.py
@@ -10,18 +10,17 @@ from simpleml.util import exportDictionaryAsJSON
 def exampleWorkflow():
 
     dataset = loadDataset("WhiteWineQualityBinary")
-    print(exportDictionaryAsJSON(dataset.getStatistics()))
+    print(exportDictionaryAsJSON(dataset.getProfile()))
 
-
-    train, test = dataset.splitIntoTrainAndTest(train_ratio=0.75)
+    train, test = dataset.splitIntoTrainAndTest(trainRatio=0.75)
     X_train = train.dropAttributes("quality")
-    X_test = train.dropAttributes("quality")
+    X_test = test.dropAttributes("quality")
     y_train = train.keepAttributes("quality")
 
     # compute statistics from the dataset
-    print(exportDictionaryAsJSON(X_train.getStatistics()))
-    print(exportDictionaryAsJSON(y_train.getStatistics()))
-    print(exportDictionaryAsJSON(X_test.getStatistics()))
+    print(exportDictionaryAsJSON(X_train.getProfile()))
+    print(exportDictionaryAsJSON(y_train.getProfile()))
+    print(exportDictionaryAsJSON(X_test.getProfile()))
 
 if __name__ == '__main__':
     exampleWorkflow()
