454578b6bc05d0a0629c142e86fb2e7fcb44d8f8
Lars Reimann <mail@larsreimann.com>
2021-04-26 11:27:58 +0200

    Load stdlib from JAR rather than using an absolute path

--- server/de.unibonn.simpleml.parent/de.unibonn.simpleml.ide/src/de/unibonn/simpleml/ide/SimpleMLProjectManager.kt
+++ server/de.unibonn.simpleml.parent/de.unibonn.simpleml.ide/src/de/unibonn/simpleml/ide/SimpleMLProjectManager.kt
@@ -14,46 +14,17 @@ class SimpleMLProjectManager @Inject constructor(
 ) : ProjectManager() {
 
     override fun doInitialBuild(cancelIndicator: CancelIndicator): IncrementalBuilder.Result {
+        println("Source files: ")
         val uris = projectConfig.sourceFolders
             .flatMap { srcFolder -> srcFolder.getAllResources(fileSystemScanner) }
             .toMutableList()
 
-//        val resourcesUrl = stdlib.javaClass.classLoader.getResource("simpleml")
-//        if (resourcesUrl != null) {
-//            println(resourcesUrl.toString())
-//            val resourcesUri = FileLocator.resolve(resourcesUrl).toURI()
-//            println(resourcesUri.toString())
-//            var fileSystem: FileSystem? = null
-//            val stdlibBase = when (resourcesUri.scheme) {
-//
-//                // Without this code Maven tests fail with a FileSystemNotFoundException since stdlib resources are in a jar
-//                "jar" -> {
-//                    fileSystem = FileSystems.newFileSystem(resourcesUri, emptyMap<String, String>(), null)
-//                    fileSystem.getPath("simpleml")
-//                }
-//                else -> Paths.get(resourcesUri)
-//            }
-//            println(stdlibBase)
-//
-//            Files.walk(stdlibBase)
-//                .filter { it.toString().endsWith(".stub.simpleml") }
-//                .forEach {
-//                    uris += URI.createFileURI(it.toString())
-//                }
-//        }
+        uris.forEach { println(it.toString()) }
 
-        // TODO: must be configurable within the editor + we need a solution for the web setup + standalone (tests)
-        //  think about the relation between the DSL and the stdlib stubs (one or multiple repos)
-        Files.walk(Path.of("C:\\Users\\Lars\\Documents\\Repositories\\Simple-ML\\stdlib\\stubs"))
-            .filter { it.toString().endsWith(".stub.simpleml") }
-            .forEach {
-                println(it.toString())
-                println(URI.createFileURI(it.toString()).toString())
-                uris += URI.createFileURI(it.toString())
-            }
-
-        uris.forEach {
-            println(it.toString())
+        println("Stdlib files: ")
+        stdlib.listStdlibFiles().forEach { (_, uri) ->
+            uris += uri
+            println(uri.toString())
         }
 
         return doBuild(uris, emptyList(), emptyList(), cancelIndicator)
