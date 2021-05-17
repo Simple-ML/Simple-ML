f10a8a2dce94ad1c4ecdb47a7bd5fe34609466f5
fakharabbas-3002 <fakhar_s124@yahoo.com>
2021-05-17 19:10:26 +0200

    All dataset, geo data, local files and few other changes

---
+++ tests/localDataset.py
@@ -0,0 +1,27 @@
+# Imports ----------------------------------------------------------------------
+from simpleml.data_catalog import getDatasets
+from simpleml.ml_catalog import getMLAlgorithmClasses, getMLAlgorithms, getMetrics, getBenchmarks
+from simpleml.dataset import readDataSetFromCSV
+from simpleml.util import exportDictionaryAsJSON
+
+
+# Workflow steps ---------------------------------------------------------------
+
+def exampleWorkflow():
+
+    dataset = readDataSetFromCSV('WhiteWineQualityBinary2.csv', 'New dataset')
+    print(exportDictionaryAsJSON(dataset.getProfile()))
+
+    train, test = dataset.splitIntoTrainAndTest(trainRatio=0.75, randomState=1)
+    X_train = train.dropAttributes("quality")
+    X_test = test.dropAttributes("quality")
+    y_train = train.keepAttributes("quality")
+
+    # compute statistics from the dataset
+    print(exportDictionaryAsJSON(X_train.getProfile()))
+    print(exportDictionaryAsJSON(y_train.getProfile()))
+    print(exportDictionaryAsJSON(X_test.getProfile()))
+
+
+if __name__ == '__main__':
+    exampleWorkflow()
---
+++ tests/speedAveragesBasic.py
@@ -0,0 +1,29 @@
+# Imports ----------------------------------------------------------------------
+from simpleml.data_catalog import getDatasets
+from simpleml.ml_catalog import getMLAlgorithmClasses, getMLAlgorithms, getMetrics, getBenchmarks
+from simpleml.dataset import loadDataset
+from simpleml.util import exportDictionaryAsJSON
+
+
+# Workflow steps ---------------------------------------------------------------
+
+def exampleWorkflow():
+
+    dataset = loadDataset("SpeedAveragesMiniSample")
+    print(exportDictionaryAsJSON(dataset.getProfile()))
+
+    train, test = dataset.splitIntoTrainAndTest(trainRatio=0.75, randomState=1)
+    #print(train.data)
+    X_train = train.dropAttributes("is_weekend")
+    X_test = test.dropAttributes("is_weekend")
+    y_train = train.keepAttributes("is_weekend")
+    #print(X_train.data)
+
+    # compute statistics from the dataset
+    #print('x train', X_train.getProfile())
+    print(exportDictionaryAsJSON(X_train.getProfile()))
+    print(exportDictionaryAsJSON(X_test.getProfile()))
+    print(exportDictionaryAsJSON(y_train.getProfile()))
+
+if __name__ == '__main__':
+    exampleWorkflow()
