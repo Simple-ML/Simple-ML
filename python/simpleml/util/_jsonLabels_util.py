b5969b7fc713d9bd4c11d6beecdf233f3fa5b972
Simon Gottschalk <gottschalk@l3s.de>
2021-06-10 03:23:13 +0200

    provide statistics for any data type

--- python/simpleml/util/_jsonLabels_util.py
+++ python/simpleml/util/_jsonLabels_util.py
@@ -1,14 +1,40 @@
 import numpy as np
+from shapely import geometry
+from pandas import Timestamp, Int32Dtype
 
 lang = 'de'
 type = 'type'
+i18n_id = 'id'
+
+type_numeric = 'numeric'
+type_integer = 'integer'
+type_float = 'float'
+type_string = 'string'
+type_datetime = 'datetime'
+type_bool = 'boolean'
+type_geometry = 'geometry'
+
+type_dataset = 'dataset'
 
 type_table = 'table'
 type_table_values = 'lines'
 type_table_data_types = 'data_types'
 type_table_header_labels = 'header_labels'
 
-data_type_labels = {np.float64: 'float', np.integer: 'integer'}
+type_histogram = 'histogram'
+type_histogram_buckets = 'buckets'
+data_type_labels = {np.int32: type_integer, Int32Dtype(): type_integer, np.bool_: type_bool, np.datetime64: type_datetime, str: type_string, int: type_integer, float: type_float, np.int64: type_integer, np.float64: type_float, np.floating: type_float, np.integer: type_integer, np.dtype('float64'): type_float, np.dtype('int64'): type_integer,np.dtype('bool'): type_bool, 'str': type_string, bool: type_bool, object: 'ERROR', geometry: type_geometry, Timestamp: type_datetime}
+
+type_bar_chart = 'bar_chart'
+type_bar_chart_bars = 'bars'
+
+type_list = 'list'
+type_list_values = 'values'
+type_list_data_type = 'data_type'
+
+type_box_plot = 'box_plot'
+type_box_plot_values = 'values'
+type_box_plot_data_type = 'data_type'
 
 statistics = 'statistics'
 attributes = 'attributes'
@@ -24,10 +50,10 @@ number_of_instances = 'number_of_instances'
 
 attribute_label = 'label'
 
-line_accurrence_in_areas = 'lineAccurrenceInAreas'
+spatialValueDistribution = 'spatialValueDistribution'
 numberOfNullValues = 'numberOfNullValues'
-decile = 'decile'
-quartile = 'quartile'
+deciles = 'deciles'
+quartiles = 'quartiles'
 averageNumberOfSpecialCharacters = 'averageNumberOfSpecialCharacters'
 averageNumberOfTokens = 'averageNumberOfTokens'
 numberOfValidValues = 'numberOfValidValues'
@@ -63,3 +89,26 @@ sample_lines = 'lines'
 value_distribution = 'value_distribution'
 value_distribution_number_of_instances = 'number_of_instances'
 value_distribution_value = 'value'
+
+
+type_numeric_names = {'de': {
+    'averageNumberOfSpecialCharacters': 'durchschnittliche Anzahl an Sonderzeichen',
+    'averageNumberOfTokens': 'durchschnittliche Anzahl an Wörtern',
+    'averageNumberOfCapitalisedValues': 'durchschnittliche Anzahl an Großbuchstaben',
+    'averageNumberOfCharacters': 'durchschnittliche Anzahl an Zeichen',
+    'averageNumberOfDigits': 'durchschnittliche Anzahl an Ziffern',
+    numberOfDistinctValues: 'Anzahl verschiedener Werte',
+    }, 'en': {
+    'averageNumberOfSpecialCharacters': 'average number of special characters',
+    'averageNumberOfTokens': 'average number of words',
+    'averageNumberOfCapitalisedValues': 'average number of capitalised characters',
+    'averageNumberOfCharacters': 'average number of characters',
+    'averageNumberOfDigits': 'average number of digits',
+    numberOfDistinctValues: 'number of distinct values',
+}}
+
+type_numeric_data_type = 'data_type'
+type_numeric_value = 'value'
+type_numeric_name = 'name'
+
+datetime_format = { 'en' : '%Y-%m-%d %H:%M:%S', 'de': '%d.%m.%Y, %H:%M:%S'}
