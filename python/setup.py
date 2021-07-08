9a9fb11f580ec0576217ea9489f4dae5fc2ad392
Lars Reimann <mail@larsreimann.com>
2021-07-08 18:45:03 +0000

    Update VS Code devcontainer to use pip instead of conda

---
+++ python/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(
+        name='Simple-ML',
+        version='0.0.1',
+        description='Adapters for various ML libraries',
+        packages=['simpleml']
+    )
