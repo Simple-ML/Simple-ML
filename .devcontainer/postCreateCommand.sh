6efb6734dd68384f1864fd99b0d27d1c11027a7b
Lars Reimann <mail@larsreimann.com>
2021-07-11 21:00:49 +0000

    fix: broken language server installation

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -11,7 +11,3 @@ cd server/de.unibonn.simpleml.parent
 cd ../../client
 npm i
 npm run webpack
-
-# Install pre-built preview of Simple-ML extension (remove once it's on the marketplace)
-cd ..
-code --install-extension "server/de.unibonn.simpleml.parent/de.unibonn.simpleml/stdlib/simple-ml-0.0.7.vsix"
