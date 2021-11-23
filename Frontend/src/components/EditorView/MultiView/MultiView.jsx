1657daab3712a6869e1b640bd9ad63bf8bd5a3b5
khalid-abdalla <90764020+khalid-abdalla@users.noreply.github.com>
2021-11-23 08:57:57 +0100

    feat: implement test sidebar
    accordion and search input

--- Frontend/src/components/EditorView/MultiView/MultiView.jsx
+++ Frontend/src/components/EditorView/MultiView/MultiView.jsx
@@ -4,6 +4,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import $ from "jquery";
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import TextField from '@material-ui/core/TextField';
 
 // React.Components
 import GoldenLayoutComponent from './../../../helper/goldenLayoutServices/goldenLayoutComponent';
@@ -72,6 +79,67 @@ class MultiView extends React.Component {
                         })
                     }}
                 />
+                <div style= {{backgroundColor: 'white'}}>
+                    <header style= {{backgroundColor: 'white'}}>
+                        <h1>Select data set</h1>
+                        <p>Please select a data set</p>
+                    </header> 
+                    <Autocomplete
+                        freeSolo
+                        id="free-solo-2-demo"
+                        disableClearable
+                        options={['Traffic and Geo', 'Weather']}
+                        renderInput={(params) => (
+                            <TextField
+                            style= {{backgroundColor: 'white'}}
+                                {...params}
+                                label="Search input"
+                                InputProps={{
+                                ...params.InputProps,
+                                type: 'search',
+                                }}
+                            />
+                        )}
+                    />
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1a-content"
+                            id="panel1a-header"
+                        >
+                        <Typography>Traffic and Geo</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>
+                                ADAC NRW und OSM
+                            </Typography>
+                        </AccordionDetails>
+                        <AccordionDetails>
+                            <Typography>    
+                                ADAC Niedersachsen und OSM
+                            </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1a-content"
+                            id="panel1a-header"
+                        >
+                        <Typography>Weather</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>
+                                Wetter
+                            </Typography>
+                        </AccordionDetails>
+                        <AccordionDetails>
+                            <Typography>
+                                DWD.de
+                            </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                </div>
             </div>
         )
     }
