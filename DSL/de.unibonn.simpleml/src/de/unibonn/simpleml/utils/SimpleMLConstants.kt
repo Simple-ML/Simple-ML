da1da2bc806fa9741bb9bdf977e7abb41db81ae0
Lars Reimann <mail@larsreimann.com>
2021-11-29 16:07:37 +0100

    feat: generate Python code for parenthesized expressions

--- DSL/de.unibonn.simpleml/src/de/unibonn/simpleml/utils/SimpleMLConstants.kt
+++ DSL/de.unibonn.simpleml/src/de/unibonn/simpleml/utils/SimpleMLConstants.kt
@@ -3,6 +3,7 @@ package de.unibonn.simpleml.utils
 // File extensions -----------------------------------------------------------------------------------------------------
 
 const val SML_STUB_EXTENSION = ".stub.simpleml"
+const val SML_TEST_EXTENSION = ".test.simpleml"
 const val SML_WORKFLOW_EXTENSION = ".simpleml"
 
 // Modifiers -----------------------------------------------------------------------------------------------------------
