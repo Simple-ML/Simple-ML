10a2d9dc6089614a9d5c6bcde5bcc9549801ab2e
Lars Reimann <mail@larsreimann.com>
2021-12-03 20:21:32 +0100

    test: stubs in stdlib (#94)
    * test: stdlib should not have errors
    
    * fix: errors in stdlib
    
    * refactor: rename a function
    
    * test: check stdlib files have no warnings either
    
    * fix: lint error
    
    * style: apply automatic fixes of linters
    
    Co-authored-by: lars-reimann <lars-reimann@users.noreply.github.com>

--- DSL/de.unibonn.simpleml/src/de/unibonn/simpleml/utils/SimpleMLStdlib.kt
+++ DSL/de.unibonn.simpleml/src/de/unibonn/simpleml/utils/SimpleMLStdlib.kt
@@ -6,14 +6,18 @@ import org.eclipse.core.runtime.FileLocator
 import org.eclipse.emf.common.util.URI
 import org.eclipse.emf.ecore.EObject
 import org.eclipse.emf.ecore.resource.ResourceSet
-import java.nio.file.*
+import java.nio.file.FileSystem
+import java.nio.file.FileSystems
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
 
 private const val LIB_PACKAGE = "simpleml.lang"
-const val LIB_ANY = "${LIB_PACKAGE}.Any"
-const val LIB_BOOLEAN = "${LIB_PACKAGE}.Boolean"
-const val LIB_FLOAT = "${LIB_PACKAGE}.Float"
-const val LIB_INT = "${LIB_PACKAGE}.Int"
-const val LIB_STRING = "${LIB_PACKAGE}.String"
+const val LIB_ANY = "$LIB_PACKAGE.Any"
+const val LIB_BOOLEAN = "$LIB_PACKAGE.Boolean"
+const val LIB_FLOAT = "$LIB_PACKAGE.Float"
+const val LIB_INT = "$LIB_PACKAGE.Int"
+const val LIB_STRING = "$LIB_PACKAGE.String"
 
 class SimpleMLStdlib @Inject constructor(
     private val indexExtensions: SimpleMLIndexExtensions
@@ -53,7 +57,8 @@ class SimpleMLStdlib @Inject constructor(
 
                 // Without this code Maven tests fail with a FileSystemNotFoundException since stdlib resources are in a jar
                 "jar" -> {
-                    fileSystem = FileSystems.newFileSystem(resourcesUri, emptyMap<String, String>(), null)
+                    fileSystem =
+                        FileSystems.newFileSystem(resourcesUri, emptyMap<String, String>(), null)
                     fileSystem.getPath("stubs")
                 }
                 else -> Paths.get(resourcesUri)
