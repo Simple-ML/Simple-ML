c339dd08854a7c59858a99981b76d99cf6c03c45
Lars Reimann <mail@larsreimann.com>
2021-12-06 17:07:11 +0100

    feat: implement for annotations

--- DSL/de.unibonn.simpleml/src/main/kotlin/de/unibonn/simpleml/validation/declarations/AnnotationChecker.kt
+++ DSL/de.unibonn.simpleml/src/main/kotlin/de/unibonn/simpleml/validation/declarations/AnnotationChecker.kt
@@ -1,9 +1,15 @@
 package de.unibonn.simpleml.validation.declarations
 
 import de.unibonn.simpleml.emf.parametersOrEmpty
+import de.unibonn.simpleml.simpleML.SimpleMLPackage
 import de.unibonn.simpleml.simpleML.SmlAnnotation
+import de.unibonn.simpleml.simpleML.SmlAnnotationUse
+import de.unibonn.simpleml.utils.parametersOrNull
 import de.unibonn.simpleml.validation.AbstractSimpleMLChecker
+import de.unibonn.simpleml.validation.other.UNNECESSARY_ARGUMENT_LIST
 import org.eclipse.xtext.validation.Check
+
+const val UNNECESSARY_PARAMETER_LIST = "UNNECESSARY_PARAMETER_LIST"
 
 class AnnotationChecker : AbstractSimpleMLChecker() {
 
@@ -11,6 +17,17 @@ class AnnotationChecker : AbstractSimpleMLChecker() {
     fun uniqueNames(smlAnnotation: SmlAnnotation) {
         smlAnnotation.parametersOrEmpty().reportDuplicateNames {
             "A parameter with name '${it.name}' exists already in this annotation."
+        }
+    }
+
+    @Check
+    fun unnecessaryParameterList(smlAnnotation: SmlAnnotation) {
+        if (smlAnnotation.parameterList != null && smlAnnotation.parametersOrEmpty().isEmpty()) {
+            warning(
+                "Unnecessary parameter list.",
+                SimpleMLPackage.Literals.SML_ANNOTATION__PARAMETER_LIST,
+                UNNECESSARY_PARAMETER_LIST
+            )
         }
     }
 }
