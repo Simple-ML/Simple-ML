4f82be74990e3bc6e6f7ddefba424068cefc772c
Lars Reimann <mail@larsreimann.com>
2021-12-06 17:12:46 +0100

    feat: implement for workflow steps

--- DSL/de.unibonn.simpleml/src/main/kotlin/de/unibonn/simpleml/validation/declarations/WorkflowStepChecker.kt
+++ DSL/de.unibonn.simpleml/src/main/kotlin/de/unibonn/simpleml/validation/declarations/WorkflowStepChecker.kt
@@ -3,13 +3,16 @@ package de.unibonn.simpleml.validation.declarations
 import de.unibonn.simpleml.emf.parametersOrEmpty
 import de.unibonn.simpleml.emf.placeholdersOrEmpty
 import de.unibonn.simpleml.emf.resultsOrEmpty
+import de.unibonn.simpleml.emf.typeParametersOrEmpty
 import de.unibonn.simpleml.simpleML.SimpleMLPackage.Literals
+import de.unibonn.simpleml.simpleML.SmlClass
 import de.unibonn.simpleml.simpleML.SmlWorkflowStep
 import de.unibonn.simpleml.utils.usesIn
 import de.unibonn.simpleml.validation.AbstractSimpleMLChecker
 import org.eclipse.xtext.validation.Check
 
 const val PARAMETER_IS_UNUSED = "PARAMETER_IS_UNUSED"
+const val UNNECESSARY_RESULT_LIST = "UNNECESSARY_RESULT_LIST"
 
 class WorkflowStepChecker : AbstractSimpleMLChecker() {
 
@@ -33,6 +36,17 @@ class WorkflowStepChecker : AbstractSimpleMLChecker() {
             smlWorkflowStep.parametersOrEmpty() + smlWorkflowStep.resultsOrEmpty() + smlWorkflowStep.placeholdersOrEmpty()
         declarations.reportDuplicateNames {
             "A parameter, result or placeholder with name '${it.name}' exists already in this workflow step."
+        }
+    }
+
+    @Check
+    fun unnecessaryResultList(smlWorkflowStep: SmlWorkflowStep) {
+        if (smlWorkflowStep.resultList != null && smlWorkflowStep.resultsOrEmpty().isEmpty()) {
+            warning(
+                "Unnecessary result list.",
+                Literals.SML_WORKFLOW_STEP__RESULT_LIST,
+                UNNECESSARY_RESULT_LIST
+            )
         }
     }
 }
