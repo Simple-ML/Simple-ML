7cab27388a74cdab1bbea821f032c86734862ca3
Lars Reimann <mail@larsreimann.com>
2021-12-09 20:17:36 +0100

    feat: symbols for workflow steps

--- DSL/de.unibonn.simpleml.ide/src/main/kotlin/de/unibonn/simpleml/ide/symbol/SimpleMLDocumentSymbolKindProvider.kt
+++ DSL/de.unibonn.simpleml.ide/src/main/kotlin/de/unibonn/simpleml/ide/symbol/SimpleMLDocumentSymbolKindProvider.kt
@@ -8,9 +8,11 @@ import org.eclipse.xtext.ide.server.symbol.DocumentSymbolMapper
 class SimpleMLDocumentSymbolKindProvider : DocumentSymbolMapper.DocumentSymbolKindProvider() {
     override fun getSymbolKind(clazz: EClass): SymbolKind {
         return when (clazz) {
+            Literals.SML_COMPILATION_UNIT -> SymbolKind.File
             Literals.SML_ENUM -> SymbolKind.Enum
             Literals.SML_ENUM_VARIANT -> SymbolKind.EnumMember
             Literals.SML_WORKFLOW -> SymbolKind.Function
+            Literals.SML_WORKFLOW_STEP -> SymbolKind.Function
             else -> super.getSymbolKind(clazz)
         }
     }
