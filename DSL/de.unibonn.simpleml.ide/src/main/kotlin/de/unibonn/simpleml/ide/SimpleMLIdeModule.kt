d1c51e951d463095f23075bd8e74921dace670da
Lars Reimann <mail@larsreimann.com>
2021-12-09 19:09:24 +0100

    feat: mark symbols as deprecated

--- DSL/de.unibonn.simpleml.ide/src/main/kotlin/de/unibonn/simpleml/ide/SimpleMLIdeModule.kt
+++ DSL/de.unibonn.simpleml.ide/src/main/kotlin/de/unibonn/simpleml/ide/SimpleMLIdeModule.kt
@@ -1,18 +1,24 @@
 package de.unibonn.simpleml.ide
 
 import de.unibonn.simpleml.ide.contentassist.SimpleMLIdeContentProposalProvider
+import de.unibonn.simpleml.ide.symbol.SimpleMLDocumentSymbolDeprecationInfoProvider
 import org.eclipse.xtext.ide.editor.contentassist.IdeContentProposalProvider
 import org.eclipse.xtext.ide.server.hover.HoverService
+import org.eclipse.xtext.ide.server.symbol.DocumentSymbolMapper
 
 /**
  * Use this class to register ide components.
  */
 class SimpleMLIdeModule : AbstractSimpleMLIdeModule() {
-    fun bindIdeContentProposalProvider(): Class<out IdeContentProposalProvider?> {
+    fun bindDocumentSymbolDeprecationInfoProvider(): Class<out DocumentSymbolMapper.DocumentSymbolDeprecationInfoProvider> {
+        return SimpleMLDocumentSymbolDeprecationInfoProvider::class.java
+    }
+
+    fun bindIdeContentProposalProvider(): Class<out IdeContentProposalProvider> {
         return SimpleMLIdeContentProposalProvider::class.java
     }
 
-    fun bindHoverService(): Class<out HoverService?> {
+    fun bindHoverService(): Class<out HoverService> {
         return SimpleMLHoverService::class.java
     }
 }
