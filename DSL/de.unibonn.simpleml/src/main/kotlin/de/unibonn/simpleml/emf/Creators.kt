ed29ca1696ada53f82bcd78c06084ab0ece536c3
Lars Reimann <mail@larsreimann.com>
2021-12-14 11:02:31 +0100

    feat: more creators

--- DSL/de.unibonn.simpleml/src/main/kotlin/de/unibonn/simpleml/emf/Creators.kt
+++ DSL/de.unibonn.simpleml/src/main/kotlin/de/unibonn/simpleml/emf/Creators.kt
@@ -1,3 +1,5 @@
+@file:Suppress("unused")
+
 package de.unibonn.simpleml.emf
 
 import de.unibonn.simpleml.constant.FileExtension
@@ -6,64 +8,37 @@ import de.unibonn.simpleml.simpleML.SmlAbstractAssignee
 import de.unibonn.simpleml.simpleML.SmlAbstractExpression
 import de.unibonn.simpleml.simpleML.SmlAbstractStatement
 import de.unibonn.simpleml.simpleML.SmlAbstractType
-import de.unibonn.simpleml.simpleML.SmlAnnotation
 import de.unibonn.simpleml.simpleML.SmlAnnotationUse
 import de.unibonn.simpleml.simpleML.SmlAnnotationUseHolder
 import de.unibonn.simpleml.simpleML.SmlArgument
 import de.unibonn.simpleml.simpleML.SmlArgumentList
 import de.unibonn.simpleml.simpleML.SmlAssigneeList
-import de.unibonn.simpleml.simpleML.SmlAssignment
-import de.unibonn.simpleml.simpleML.SmlAttribute
 import de.unibonn.simpleml.simpleML.SmlBlock
 import de.unibonn.simpleml.simpleML.SmlBoolean
-import de.unibonn.simpleml.simpleML.SmlCall
-import de.unibonn.simpleml.simpleML.SmlCallableType
-import de.unibonn.simpleml.simpleML.SmlClass
-import de.unibonn.simpleml.simpleML.SmlClassBody
 import de.unibonn.simpleml.simpleML.SmlCompilationUnit
 import de.unibonn.simpleml.simpleML.SmlEnum
-import de.unibonn.simpleml.simpleML.SmlEnumBody
 import de.unibonn.simpleml.simpleML.SmlEnumVariant
-import de.unibonn.simpleml.simpleML.SmlExpressionStatement
 import de.unibonn.simpleml.simpleML.SmlFloat
-import de.unibonn.simpleml.simpleML.SmlFunction
-import de.unibonn.simpleml.simpleML.SmlImport
 import de.unibonn.simpleml.simpleML.SmlImportAlias
-import de.unibonn.simpleml.simpleML.SmlInfixOperation
 import de.unibonn.simpleml.simpleML.SmlInt
-import de.unibonn.simpleml.simpleML.SmlLambda
-import de.unibonn.simpleml.simpleML.SmlLambdaResult
-import de.unibonn.simpleml.simpleML.SmlMemberAccess
-import de.unibonn.simpleml.simpleML.SmlMemberType
-import de.unibonn.simpleml.simpleML.SmlNamedType
 import de.unibonn.simpleml.simpleML.SmlNull
-import de.unibonn.simpleml.simpleML.SmlPackage
 import de.unibonn.simpleml.simpleML.SmlParameter
 import de.unibonn.simpleml.simpleML.SmlParameterList
 import de.unibonn.simpleml.simpleML.SmlParentTypeList
 import de.unibonn.simpleml.simpleML.SmlParenthesizedExpression
 import de.unibonn.simpleml.simpleML.SmlParenthesizedType
-import de.unibonn.simpleml.simpleML.SmlPlaceholder
-import de.unibonn.simpleml.simpleML.SmlPrefixOperation
-import de.unibonn.simpleml.simpleML.SmlReference
 import de.unibonn.simpleml.simpleML.SmlResult
 import de.unibonn.simpleml.simpleML.SmlResultList
 import de.unibonn.simpleml.simpleML.SmlStarProjection
 import de.unibonn.simpleml.simpleML.SmlString
-import de.unibonn.simpleml.simpleML.SmlTemplateString
-import de.unibonn.simpleml.simpleML.SmlTemplateStringPart
 import de.unibonn.simpleml.simpleML.SmlTypeArgument
 import de.unibonn.simpleml.simpleML.SmlTypeArgumentList
 import de.unibonn.simpleml.simpleML.SmlTypeParameter
 import de.unibonn.simpleml.simpleML.SmlTypeParameterConstraint
 import de.unibonn.simpleml.simpleML.SmlTypeParameterConstraintList
 import de.unibonn.simpleml.simpleML.SmlTypeParameterList
-import de.unibonn.simpleml.simpleML.SmlTypeProjection
 import de.unibonn.simpleml.simpleML.SmlUnionType
 import de.unibonn.simpleml.simpleML.SmlWildcard
-import de.unibonn.simpleml.simpleML.SmlWorkflow
-import de.unibonn.simpleml.simpleML.SmlWorkflowStep
-import de.unibonn.simpleml.simpleML.SmlYield
 import org.eclipse.emf.common.util.URI
 import org.eclipse.emf.ecore.resource.Resource
 import org.eclipse.xtext.resource.XtextResource
@@ -87,25 +62,29 @@ fun createSmlDummyResource(
     }
 }
 
-/**
- * Returns a new object of class [SmlAnnotation].
- */
-fun createSmlAnnotation(): SmlAnnotation
-
-/**
- * Returns a new object of class [SmlAnnotationUse].
- */
-fun createSmlAnnotationUse(): SmlAnnotationUse
+///**
+// * Returns a new object of class [SmlAnnotation].
+// */
+//fun createSmlAnnotation(): SmlAnnotation
+//
+///**
+// * Returns a new object of class [SmlAnnotationUse].
+// */
+//fun createSmlAnnotationUse(): SmlAnnotationUse
 
 /**
  * Returns a new object of class [SmlAnnotationUseHolder].
  */
-private fun createSmlAnnotationUseHolder(): SmlAnnotationUseHolder
+private fun createSmlAnnotationUseHolder(annotations: List<SmlAnnotationUse>): SmlAnnotationUseHolder {
+    return factory.createSmlAnnotationUseHolder().apply {
+        this.annotations += annotations
+    }
+}
 
-/**
- * Returns a new object of class [SmlArgument].
- */
-fun createSmlArgument(): SmlArgument
+///**
+// * Returns a new object of class [SmlArgument].
+// */
+//fun createSmlArgument(): SmlArgument
 
 /**
  * Returns a new object of class [SmlArgumentList].
@@ -125,15 +104,15 @@ private fun createSmlAssigneeList(assignees: List<SmlAbstractAssignee>): SmlAssi
     }
 }
 
-/**
- * Returns a new object of class [SmlAssignment].
- */
-fun createSmlAssignment(): SmlAssignment
-
-/**
- * Returns a new object of class [SmlAttribute].
- */
-fun createSmlAttribute(): SmlAttribute
+///**
+// * Returns a new object of class [SmlAssignment].
+// */
+//fun createSmlAssignment(): SmlAssignment
+//
+///**
+// * Returns a new object of class [SmlAttribute].
+// */
+//fun createSmlAttribute(): SmlAttribute
 
 /**
  * Returns a new object of class [SmlBlock].
@@ -153,54 +132,86 @@ fun createSmlBoolean(value: Boolean): SmlBoolean {
     }
 }
 
-/**
- * Returns a new object of class [SmlCall].
- */
-fun createSmlCall(): SmlCall
-
-/**
- * Returns a new object of class [SmlCallableType].
- */
-fun createSmlCallableType(): SmlCallableType
-
-/**
- * Returns a new object of class [SmlClass].
- */
-fun createSmlClass(): SmlClass
-
-/**
- * Returns a new object of class [SmlClassBody].
- */
-private fun createSmlClassBody(): SmlClassBody
-
-/**
- * Returns a new object of class [SmlCompilationUnit].
- */
-fun createSmlCompilationUnit(): SmlCompilationUnit
+///**
+// * Returns a new object of class [SmlCall].
+// */
+//fun createSmlCall(): SmlCall
+//
+///**
+// * Returns a new object of class [SmlCallableType].
+// */
+//fun createSmlCallableType(): SmlCallableType
+//
+///**
+// * Returns a new object of class [SmlClass].
+// */
+//fun createSmlClass(): SmlClass
+//
+///**
+// * Returns a new object of class [SmlClassBody].
+// */
+//private fun createSmlClassBody(): SmlClassBody
+//
+///**
+// * Returns a new object of class [SmlCompilationUnit].
+// */
+//fun createSmlCompilationUnit(): SmlCompilationUnit
 
 /**
  * Returns a new object of class [SmlEnum].
  */
-fun createSmlEnum(): SmlEnum
-
-/**
- * Returns a new object of class [SmlEnumBody].
- */
-private fun createSmlEnumBody(variants: List<SmlEnumVariant>): SmlEnumBody {
-    return factory.createSmlEnumBody().apply {
-        this.variants += variants
+fun createSmlEnum(
+    name: String,
+    annotations: List<SmlAnnotationUse> = emptyList(),
+    init: SmlEnum.() -> Unit
+): SmlEnum {
+    return factory.createSmlEnum().apply {
+        this.name = name
+        this.annotationUseHolder = createSmlAnnotationUseHolder(annotations)
+        this.init()
     }
 }
 
 /**
  * Returns a new object of class [SmlEnumVariant].
  */
-fun createSmlEnumVariant(): SmlEnumVariant
+fun createSmlEnumVariant(
+    name: String,
+    annotations: List<SmlAnnotationUse> = emptyList(),
+    typeParameters: List<SmlTypeParameter>? = null,
+    parameters: List<SmlParameter>? = null,
+    typeParameterConstraints: List<SmlTypeParameterConstraint>? = null
+): SmlEnumVariant {
+    return factory.createSmlEnumVariant().apply {
+        this.name = name
+        this.annotations += annotations
+        this.typeParameterList = createSmlTypeParameterList(typeParameters)
+        this.parameterList = createSmlParameterList(parameters)
+        this.typeParameterConstraintList = createSmlTypeParameterConstraintList(typeParameterConstraints)
+    }
+}
 
 /**
- * Returns a new object of class [SmlExpressionStatement].
+ * Adds a new object of class [SmlEnumVariant] to the receiver.
  */
-fun createSmlExpressionStatement(): SmlExpressionStatement
+fun SmlEnum.smlEnumVariant(
+    name: String,
+    annotations: List<SmlAnnotationUse> = emptyList(),
+    typeParameters: List<SmlTypeParameter> = emptyList(),
+    parameters: List<SmlParameter> = emptyList(),
+    typeParameterConstraint: List<SmlTypeParameterConstraint> = emptyList()
+) {
+    if (this.body == null) {
+        this.body = factory.createSmlEnumBody()
+    }
+
+    this.body.variants += createSmlEnumVariant(name, annotations, typeParameters, parameters, typeParameterConstraint)
+}
+
+///**
+// * Returns a new object of class [SmlExpressionStatement].
+// */
+//fun createSmlExpressionStatement(): SmlExpressionStatement
 
 /**
  * Returns a new object of class [SmlFloat].
@@ -211,15 +222,15 @@ fun createSmlFloat(value: Double): SmlFloat {
     }
 }
 
-/**
- * Returns a new object of class [SmlFunction].
- */
-fun createSmlFunction(): SmlFunction
-
-/**
- * Returns a new object of class [SmlImport].
- */
-fun createSmlImport(): SmlImport
+///**
+// * Returns a new object of class [SmlFunction].
+// */
+//fun createSmlFunction(): SmlFunction
+//
+///**
+// * Returns a new object of class [SmlImport].
+// */
+//fun createSmlImport(): SmlImport
 
 /**
  * Returns a new object of class [SmlImportAlias].
@@ -230,10 +241,10 @@ private fun createSmlImportAlias(name: String): SmlImportAlias {
     }
 }
 
-/**
- * Returns a new object of class [SmlInfixOperation].
- */
-fun createSmlInfixOperation(): SmlInfixOperation
+///**
+// * Returns a new object of class [SmlInfixOperation].
+// */
+//fun createSmlInfixOperation(): SmlInfixOperation
 
 /**
  * Returns a new object of class [SmlInt].
@@ -244,30 +255,30 @@ fun createSmlInt(value: Int): SmlInt {
     }
 }
 
-/**
- * Returns a new object of class [SmlLambda].
- */
-fun createSmlLambda(): SmlLambda
-
-/**
- * Returns a new object of class [SmlLambdaResult].
- */
-fun createSmlLambdaResult(): SmlLambdaResult
-
-/**
- * Returns a new object of class [SmlMemberAccess].
- */
-fun createSmlMemberAccess(): SmlMemberAccess
-
-/**
- * Returns a new object of class [SmlMemberType].
- */
-fun createSmlMemberType(): SmlMemberType
-
-/**
- * Returns a new object of class [SmlNamedType].
- */
-fun createSmlNamedType(): SmlNamedType
+///**
+// * Returns a new object of class [SmlLambda].
+// */
+//fun createSmlLambda(): SmlLambda
+//
+///**
+// * Returns a new object of class [SmlLambdaResult].
+// */
+//fun createSmlLambdaResult(): SmlLambdaResult
+//
+///**
+// * Returns a new object of class [SmlMemberAccess].
+// */
+//fun createSmlMemberAccess(): SmlMemberAccess
+//
+///**
+// * Returns a new object of class [SmlMemberType].
+// */
+//fun createSmlMemberType(): SmlMemberType
+//
+///**
+// * Returns a new object of class [SmlNamedType].
+// */
+//fun createSmlNamedType(): SmlNamedType
 
 /**
  * Returns a new object of class [SmlNull].
@@ -276,20 +287,24 @@ fun createSmlNull(): SmlNull {
     return factory.createSmlNull()
 }
 
-/**
- * Returns a new object of class [SmlPackage].
- */
-fun createSmlPackage(): SmlPackage
+///**
+// * Returns a new object of class [SmlPackage].
+// */
+//fun createSmlPackage(): SmlPackage
+//
+///**
+// * Returns a new object of class [SmlParameter].
+// */
+//fun createSmlParameter(): SmlParameter
 
 /**
- * Returns a new object of class [SmlParameter].
+ * Returns a new object of class [SmlParameterList] or `null` if the parameter is `null`.
  */
-fun createSmlParameter(): SmlParameter
+private fun createSmlParameterList(parameters: List<SmlParameter>?): SmlParameterList? {
+    if (parameters == null) {
+        return null
+    }
 
-/**
- * Returns a new object of class [SmlParameterList].
- */
-private fun createSmlParameterList(parameters: List<SmlParameter>): SmlParameterList {
     return factory.createSmlParameterList().apply {
         this.parameters += parameters
     }
@@ -322,25 +337,25 @@ private fun createSmlParentTypeList(parentTypes: List<SmlAbstractType>): SmlPare
     }
 }
 
-/**
- * Returns a new object of class [SmlPlaceholder].
- */
-fun createSmlPlaceholder(): SmlPlaceholder
-
-/**
- * Returns a new object of class [SmlPrefixOperation].
- */
-fun createSmlPrefixOperation(): SmlPrefixOperation
-
-/**
- * Returns a new object of class [SmlReference].
- */
-fun createSmlReference(): SmlReference
-
-/**
- * Returns a new object of class [SmlResult].
- */
-fun createSmlResult(): SmlResult
+///**
+// * Returns a new object of class [SmlPlaceholder].
+// */
+//fun createSmlPlaceholder(): SmlPlaceholder
+//
+///**
+// * Returns a new object of class [SmlPrefixOperation].
+// */
+//fun createSmlPrefixOperation(): SmlPrefixOperation
+//
+///**
+// * Returns a new object of class [SmlReference].
+// */
+//fun createSmlReference(): SmlReference
+//
+///**
+// * Returns a new object of class [SmlResult].
+// */
+//fun createSmlResult(): SmlResult
 
 /**
  * Returns a new object of class [SmlResultList].
@@ -354,7 +369,9 @@ private fun createSmlResultList(results: List<SmlResult>): SmlResultList {
 /**
  * Returns a new object of class [SmlStarProjection].
  */
-fun createSmlStarProjection(): SmlStarProjection
+fun createSmlStarProjection(): SmlStarProjection {
+    return factory.createSmlStarProjection()
+}
 
 /**
  * Returns a new object of class [SmlString].
@@ -365,20 +382,20 @@ fun createSmlString(value: String): SmlString {
     }
 }
 
-/**
- * Returns a new object of class [SmlTemplateString].
- */
-fun createSmlTemplateString(): SmlTemplateString
-
-/**
- * Returns a new object of class [SmlTemplateStringPart].
- */
-fun createSmlTemplateStringPart(): SmlTemplateStringPart
-
-/**
- * Returns a new object of class [SmlTypeArgument].
- */
-fun createSmlTypeArgument(): SmlTypeArgument
+///**
+// * Returns a new object of class [SmlTemplateString].
+// */
+//fun createSmlTemplateString(): SmlTemplateString
+//
+///**
+// * Returns a new object of class [SmlTemplateStringPart].
+// */
+//fun createSmlTemplateStringPart(): SmlTemplateStringPart
+//
+///**
+// * Returns a new object of class [SmlTypeArgument].
+// */
+//fun createSmlTypeArgument(): SmlTypeArgument
 
 /**
  * Returns a new object of class [SmlTypeArgumentList].
@@ -389,38 +406,46 @@ private fun createSmlTypeArgumentList(typeArguments: List<SmlTypeArgument>): Sml
     }
 }
 
-/**
- * Returns a new object of class [SmlTypeParameter].
- */
-fun createSmlTypeParameter(): SmlTypeParameter
+///**
+// * Returns a new object of class [SmlTypeParameter].
+// */
+//fun createSmlTypeParameter(): SmlTypeParameter
 
 /**
- * Returns a new object of class [SmlTypeParameterList].
+ * Returns a new object of class [SmlTypeParameterList] or `null` if the parameter is `null`.
  */
-private fun createSmlTypeParameterList(typeParameters: List<SmlTypeParameter>): SmlTypeParameterList {
+private fun createSmlTypeParameterList(typeParameters: List<SmlTypeParameter>?): SmlTypeParameterList? {
+    if (typeParameters == null) {
+        return null
+    }
+
     return factory.createSmlTypeParameterList().apply {
         this.typeParameters += typeParameters
     }
 }
 
-/**
- * Returns a new object of class [SmlTypeParameterConstraint].
- */
-fun createSmlTypeParameterConstraint(): SmlTypeParameterConstraint
+///**
+// * Returns a new object of class [SmlTypeParameterConstraint].
+// */
+//fun createSmlTypeParameterConstraint(): SmlTypeParameterConstraint
 
 /**
- * Returns a new object of class [SmlTypeParameterConstraintList].
+ * Returns a new object of class [SmlTypeParameterConstraintList] or `null` if the parameter is `null`.
  */
-private fun createSmlTypeParameterConstraintList(constraints: List<SmlTypeParameterConstraint): SmlTypeParameterConstraintList {
+private fun createSmlTypeParameterConstraintList(constraints: List<SmlTypeParameterConstraint>?): SmlTypeParameterConstraintList? {
+    if (constraints == null) {
+        return null
+    }
+
     return factory.createSmlTypeParameterConstraintList().apply {
         this.constraints += constraints
     }
 }
 
-/**
- * Returns a new object of class [SmlTypeProjection].
- */
-fun createSmlTypeProjection(): SmlTypeProjection
+///**
+// * Returns a new object of class [SmlTypeProjection].
+// */
+//fun createSmlTypeProjection(): SmlTypeProjection
 
 /**
  * Returns a new object of class [SmlUnionType].
@@ -438,17 +463,17 @@ fun createSmlWildcard(): SmlWildcard {
     return factory.createSmlWildcard()
 }
 
-/**
- * Returns a new object of class [SmlWorkflow].
- */
-fun createSmlWorkflow(): SmlWorkflow
-
-/**
- * Returns a new object of class [SmlWorkflowStep].
- */
-fun createSmlWorkflowStep(): SmlWorkflowStep
-
-/**
- * Returns a new object of class [SmlYield].
- */
-fun createSmlYield(): SmlYield
+///**
+// * Returns a new object of class [SmlWorkflow].
+// */
+//fun createSmlWorkflow(): SmlWorkflow
+//
+///**
+// * Returns a new object of class [SmlWorkflowStep].
+// */
+//fun createSmlWorkflowStep(): SmlWorkflowStep
+//
+///**
+// * Returns a new object of class [SmlYield].
+// */
+//fun createSmlYield(): SmlYield
