a4e52b08d613ce1208ae4140e2cbe1ae89094d54
Lars Reimann <mail@larsreimann.com>
2021-12-22 16:03:47 +0100

    feat: check that subterms have unique names

--- DSL/de.unibonn.simpleml/src/main/kotlin/de/unibonn/simpleml/validation/other/ProtocolChecker.kt
+++ DSL/de.unibonn.simpleml/src/main/kotlin/de/unibonn/simpleml/validation/other/ProtocolChecker.kt
@@ -3,6 +3,7 @@ package de.unibonn.simpleml.validation.other
 import de.unibonn.simpleml.simpleML.SmlAttribute
 import de.unibonn.simpleml.simpleML.SmlFunction
 import de.unibonn.simpleml.simpleML.SmlProtocolReference
+import de.unibonn.simpleml.simpleML.SmlProtocolSubtermList
 import de.unibonn.simpleml.validation.AbstractSimpleMLChecker
 import de.unibonn.simpleml.validation.codes.ErrorCode
 import org.eclipse.xtext.validation.Check
@@ -21,6 +22,13 @@ class ProtocolChecker : AbstractSimpleMLChecker() {
                 null,
                 ErrorCode.OnlyReferenceInstanceMembers
             )
+        }
+    }
+
+    @Check
+    fun uniqueNames(smlProtocolSubtermList: SmlProtocolSubtermList) {
+        smlProtocolSubtermList.subterms.reportDuplicateNames {
+            "A subterm with name '${it.name}' exists already in this protocol."
         }
     }
 }
