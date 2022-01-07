cf79b30c96cec2056d68a84b9a3d103b560be536
Lars Reimann <mail@larsreimann.com>
2022-01-07 12:34:15 +0100

    feat: add expression lambdas

--- DSL/de.unibonn.simpleml/src/main/kotlin/de/unibonn/simpleml/validation/expressions/LambdaChecker.kt
+++ DSL/de.unibonn.simpleml/src/main/kotlin/de/unibonn/simpleml/validation/expressions/LambdaChecker.kt
@@ -3,10 +3,8 @@ package de.unibonn.simpleml.validation.expressions
 import de.unibonn.simpleml.emf.lambdaResultsOrEmpty
 import de.unibonn.simpleml.emf.parametersOrEmpty
 import de.unibonn.simpleml.emf.placeholdersOrEmpty
-import de.unibonn.simpleml.simpleML.SimpleMLPackage.Literals
 import de.unibonn.simpleml.simpleML.SmlBlockLambda
 import de.unibonn.simpleml.validation.AbstractSimpleMLChecker
-import de.unibonn.simpleml.validation.codes.InfoCode
 import org.eclipse.xtext.validation.Check
 
 class LambdaChecker : AbstractSimpleMLChecker() {
@@ -17,17 +15,6 @@ class LambdaChecker : AbstractSimpleMLChecker() {
             smlBlockLambda.parametersOrEmpty() + smlBlockLambda.placeholdersOrEmpty() + smlBlockLambda.lambdaResultsOrEmpty()
         declarations.reportDuplicateNames {
             "A parameter, result or placeholder with name '${it.name}' exists already in this lambda."
-        }
-    }
-
-    @Check
-    fun unnecessaryParameterList(smlBlockLambda: SmlBlockLambda) {
-        if (smlBlockLambda.parameterList != null && smlBlockLambda.parametersOrEmpty().isEmpty()) {
-            info(
-                "Unnecessary parameter list.",
-                Literals.SML_BLOCK_LAMBDA__PARAMETER_LIST,
-                InfoCode.UnnecessaryParameterList
-            )
         }
     }
 }
