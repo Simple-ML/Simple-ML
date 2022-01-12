0a421ac3421b276d6d98d27badb7ba84e843ee1e
sgottsch <simon-gottschalk@kabelmail.de>
2022-01-12 17:41:11 +0100

    * feat: add getRow function

--- Runtime/stdlib/test_scripts/winebasic_filter.py
+++ Runtime/stdlib/test_scripts/winebasic_filter.py
@@ -4,31 +4,17 @@ from simpleml.util import exportDictionaryAsJSON
 
 # Workflow steps ---------------------------------------------------------------
 
+
 def exampleWorkflow():
     dataset = loadDataset("WhiteWineQuality")
-    #print(exportDictionaryAsJSON(dataset.getProfile()))
 
-#    dataset_filtered = dataset.filterByAttribute(attribute='quality', value=6)
-#    print(dataset_filtered.data)
+    def filterByQuality(instance: Instance):
+        return instance.getValue("quality") == 8 or instance.getValue("quality") < 4
 
-    def filterGoodQuality(instance: Instance):
-        return instance.getAttribute("quality") == 8 or instance.getAttribute("quality") < 4
+    dataset_filtered = dataset.filterInstances(filter_func=filterByQuality)
 
-    dataset_filtered = dataset.filterInstances(filter_func = filterGoodQuality)
-    print(dataset_filtered.data)
-
-    #for index, row in dataset_filtered.data.iterrows():
-    #    print(row['quality'])
-
-    #train, test = dataset.splitIntoTrainAndTest(trainRatio=0.75, randomState=1)
-    #X_train = train.dropAttributes("quality")
-    #X_test = test.dropAttributes("quality")
-    #y_train = train.keepAttributes("quality")
-
-    # compute statistics from the dataset
-    #print(exportDictionaryAsJSON(X_train.getProfile()))
-    #print(exportDictionaryAsJSON(y_train.getProfile()))
-    #print(exportDictionaryAsJSON(X_test.getProfile()))
+    print("Example quality value:", dataset_filtered.getRow(
+        3).getAttribute("quality"))
 
 
 if __name__ == '__main__':
