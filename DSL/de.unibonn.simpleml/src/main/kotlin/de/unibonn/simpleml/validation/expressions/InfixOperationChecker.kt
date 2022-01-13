bf1c36ca0959480a30a5d21d5ecd8265a51a2d1e
Lars Reimann <mail@larsreimann.com>
2022-01-13 12:00:15 +0100

    feat: info if left operand of elvis operator is always null

--- DSL/de.unibonn.simpleml/src/main/kotlin/de/unibonn/simpleml/validation/expressions/InfixOperationChecker.kt
+++ DSL/de.unibonn.simpleml/src/main/kotlin/de/unibonn/simpleml/validation/expressions/InfixOperationChecker.kt
@@ -2,6 +2,8 @@ package de.unibonn.simpleml.validation.expressions
 
 import com.google.inject.Inject
 import de.unibonn.simpleml.constant.SmlInfixOperationOperator.Elvis
+import de.unibonn.simpleml.partialEvaluation.SmlConstantNull
+import de.unibonn.simpleml.partialEvaluation.toConstantExpressionOrNull
 import de.unibonn.simpleml.simpleML.SmlInfixOperation
 import de.unibonn.simpleml.typing.NamedType
 import de.unibonn.simpleml.typing.TypeComputer
@@ -24,7 +26,17 @@ class InfixOperationChecker @Inject constructor(
         val type = typeComputer.typeOf(smlInfixOperation.leftOperand)
         if (!(type is NamedType && type.isNullable)) {
             info(
-                "The left operand is never null so the elvis operator is unnecessary.",
+                "The left operand is never null so the elvis operator is unnecessary (keep left operand).",
+                null,
+                InfoCode.UnnecessaryElvisOperator
+            )
+            return
+        }
+
+        val value = smlInfixOperation.leftOperand.toConstantExpressionOrNull()
+        if (value is SmlConstantNull) {
+            info(
+                "The left operand is always null so the elvis operator is unnecessary (keep right operand).",
                 null,
                 InfoCode.UnnecessaryElvisOperator
             )
