92e65b49d3b10ab7400a8bd08b8c7b6a6874785e
Lars Reimann <mail@larsreimann.com>
2022-01-13 12:45:45 +0100

    feat: info if elvis operator can be simplified (#156)
    * feat: info if left operand of elvis operator is always null
    
    * feat: info if right operand of elvis operator is always null
    
    * style: apply automatic fixes of linters
    
    Co-authored-by: lars-reimann <lars-reimann@users.noreply.github.com>

--- DSL/de.unibonn.simpleml/src/main/kotlin/de/unibonn/simpleml/validation/expressions/InfixOperationChecker.kt
+++ DSL/de.unibonn.simpleml/src/main/kotlin/de/unibonn/simpleml/validation/expressions/InfixOperationChecker.kt
@@ -2,6 +2,8 @@ package de.unibonn.simpleml.validation.expressions
 
 import com.google.inject.Inject
 import de.unibonn.simpleml.constant.SmlInfixOperationOperator.Elvis
+import de.unibonn.simpleml.partialEvaluation.SmlConstantNull
+import de.unibonn.simpleml.partialEvaluation.toConstantExpressionOrNull
 import de.unibonn.simpleml.simpleML.SmlInfixOperation
 import de.unibonn.simpleml.typing.NamedType
 import de.unibonn.simpleml.typing.TypeComputer
@@ -21,10 +23,33 @@ class InfixOperationChecker @Inject constructor(
     }
 
     private fun checkElvisOperator(smlInfixOperation: SmlInfixOperation) {
-        val type = typeComputer.typeOf(smlInfixOperation.leftOperand)
-        if (!(type is NamedType && type.isNullable)) {
+        val leftType = typeComputer.typeOf(smlInfixOperation.leftOperand)
+        if (!(leftType is NamedType && leftType.isNullable)) {
             info(
-                "The left operand is never null so the elvis operator is unnecessary.",
+                "The left operand is never null so the elvis operator is unnecessary (keep left operand).",
+                null,
+                InfoCode.UnnecessaryElvisOperator
+            )
+            return
+        }
+
+        val leftValue = smlInfixOperation.leftOperand.toConstantExpressionOrNull()
+        val rightValue = smlInfixOperation.rightOperand.toConstantExpressionOrNull()
+        if (leftValue is SmlConstantNull && rightValue is SmlConstantNull) {
+            info(
+                "Both operands are always null so the elvis operator is unnecessary (replace with null).",
+                null,
+                InfoCode.UnnecessaryElvisOperator
+            )
+        } else if (leftValue is SmlConstantNull) {
+            info(
+                "The left operand is always null so the elvis operator is unnecessary (keep right operand).",
+                null,
+                InfoCode.UnnecessaryElvisOperator
+            )
+        } else if (rightValue is SmlConstantNull) {
+            info(
+                "The right operand is always null so the elvis operator is unnecessary (keep left operand).",
                 null,
                 InfoCode.UnnecessaryElvisOperator
             )
