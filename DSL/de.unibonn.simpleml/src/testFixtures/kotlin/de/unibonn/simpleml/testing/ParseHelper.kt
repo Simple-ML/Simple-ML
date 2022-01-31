b1f45c0613e1bd4155c3cb1d6b8e581636f21eb2
Lars Reimann <mail@larsreimann.com>
2022-01-31 16:57:29 +0100

    docs: document parse helpers

--- DSL/de.unibonn.simpleml/src/testFixtures/kotlin/de/unibonn/simpleml/testing/ParseHelper.kt
+++ DSL/de.unibonn.simpleml/src/testFixtures/kotlin/de/unibonn/simpleml/testing/ParseHelper.kt
@@ -8,35 +8,82 @@ import de.unibonn.simpleml.simpleML.SmlCompilationUnit
 import de.unibonn.simpleml.stdlibAccess.loadStdlib
 import org.eclipse.core.runtime.FileLocator
 import org.eclipse.emf.common.util.URI
+import org.eclipse.emf.ecore.resource.Resource
 import org.eclipse.emf.ecore.resource.ResourceSet
 import org.eclipse.xtext.testing.util.ParseHelper
 import java.nio.file.Files
 import java.nio.file.Paths
 
+/**
+ * Relative path under the `resources` folder of the test source set.
+ */
 typealias ResourceName = String
 
+/**
+ * Utilities for tests that load their test data from files or strings.
+ */
 class ParseHelper @Inject constructor(
     private val parseHelper: ParseHelper<SmlCompilationUnit>,
     private val resourceSetProvider: Provider<ResourceSet>
 ) {
 
-    fun parseResource(resourceName: ResourceName, context: List<ResourceName> = emptyList()): SmlCompilationUnit? {
+    /**
+     * Parses the contents of the resource with the given [resourceName] and returns the contained [SmlCompilationUnit]
+     * or `null` if something went wrong. Any resources in the [context] are included in the same [ResourceSet].
+     * Likewise, if [loadStdlib] is `true`, the standard library is also included in the [ResourceSet].
+     */
+    fun parseResource(
+        resourceName: ResourceName,
+        context: List<ResourceName> = emptyList(),
+        loadStdlib: Boolean = true
+    ): SmlCompilationUnit? {
+
         val programText = readProgramTextFromResource(resourceName) ?: return null
-        val resourceSet = createResourceSetFromContext(context)
-        resourceSet.loadStdlib()
-        return parseHelper.parse(
-            programText,
-            javaClass.classLoader.getResourceEmfUri(resourceName),
-            resourceSet
-        )
+        val uri = javaClass.classLoader.getResourceEmfUri(resourceName) ?: return null
+        return parseProgramText(programText, uri, context, loadStdlib)
     }
 
-    fun parseProgramText(programText: String, context: List<ResourceName> = emptyList()): SmlCompilationUnit? {
+    /**
+     * Parses the given [programText] and returns the contained [SmlCompilationUnit] or `null` if something went wrong.
+     * Any resources in the [context] are included in the same [ResourceSet]. Likewise, if [loadStdlib] is `true`, the
+     * standard library is also included in the [ResourceSet].
+     */
+    fun parseProgramText(
+        programText: String,
+        context: List<ResourceName> = emptyList(),
+        loadStdlib: Boolean = true
+    ): SmlCompilationUnit? {
+
         val resourceSet = createResourceSetFromContext(context)
-        resourceSet.loadStdlib()
+        if (loadStdlib) {
+            resourceSet.loadStdlib()
+        }
         return parseHelper.parse(programText, resourceSet)
     }
 
+    /**
+     * Parses the given [programText] and returns the contained [SmlCompilationUnit] or `null` if something went wrong.
+     * The [URI] of the [Resource] that contains the created [SmlCompilationUnit] is set to [uriToUse]. Any resources in
+     * the [context] are included in the same [ResourceSet]. Likewise, if [loadStdlib] is `true`, the standard library
+     * is also included in the [ResourceSet].
+     */
+    fun parseProgramText(
+        programText: String,
+        uriToUse: URI,
+        context: List<ResourceName> = emptyList(),
+        loadStdlib: Boolean = true
+    ): SmlCompilationUnit? {
+
+        val resourceSet = createResourceSetFromContext(context)
+        if (loadStdlib) {
+            resourceSet.loadStdlib()
+        }
+        return parseHelper.parse(programText, uriToUse, resourceSet)
+    }
+
+    /**
+     * Returns the program text within the resource with the given [resourceName] or `null` if reading failed.
+     */
     private fun readProgramTextFromResource(resourceName: ResourceName): String? {
         val resourcePath = javaClass.classLoader.getResourcePath(resourceName) ?: return null
         if (!Files.isReadable(resourcePath)) {
@@ -46,6 +93,9 @@ class ParseHelper @Inject constructor(
         return Files.readString(resourcePath)
     }
 
+    /**
+     * Creates a [ResourceSet] containing all resources in the [context].
+     */
     private fun createResourceSetFromContext(context: List<ResourceName>): ResourceSet {
         val result = resourceSetProvider.get()
         for (resourceName in context) {
