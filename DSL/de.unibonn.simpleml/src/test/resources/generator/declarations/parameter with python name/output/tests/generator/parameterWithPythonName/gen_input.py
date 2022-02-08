adc66547abe4d44d2be6ddf618dd73dc4e001c7f
Lars Reimann <mail@larsreimann.com>
2022-02-08 16:04:31 +0100

    test: update codegen tests

--- DSL/de.unibonn.simpleml/src/test/resources/generator/declarations/parameter with python name/output/tests/generator/parameterWithPythonName/gen_input.py
+++ DSL/de.unibonn.simpleml/src/test/resources/generator/declarations/parameter with python name/output/tests/generator/parameterWithPythonName/gen_input.py
@@ -1,7 +1,13 @@
 # Steps ------------------------------------------------------------------------
 
-def test(param1, param_2, param_3=0, *param_4):
-    f(lambda param1, param_2, param_3=0, *param_4: 1)
-    def __block_lambda_0(param1, param_2, param_3=0, *param_4):
+def test1(param1, param_2, param_3=0):
+    f(lambda param1, param_2, param_3=0: 1)
+    def __block_lambda_0(param1, param_2, param_3=0):
+        pass
+    f(__block_lambda_0)
+
+def test2(param1, param_2, *param_4):
+    f(lambda param1, param_2, *param_4: 1)
+    def __block_lambda_0(param1, param_2, *param_4):
         pass
     f(__block_lambda_0)
