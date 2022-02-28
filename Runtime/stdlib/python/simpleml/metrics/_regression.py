db9919c1a5352b9061bd1cbbc04dc82edfa075d8
Pascal Stammer <pstammer@agile-im.de>
2022-02-28 18:50:00 +0100

    SML-266: add regression and classification models and metric

--- Runtime/stdlib/python/simpleml/metrics/_regression.py
+++ Runtime/stdlib/python/simpleml/metrics/_regression.py
@@ -2,17 +2,17 @@ from simpleml.model.supervised._domain import DataType
 
 import sklearn.metrics as skMetrics
 
-def meanAbsoluteError(y_true: DataType, y_pred: DataType):
-    return skMetrics.mean_absolute_error(y_true.toArray(), y_pred.toArray())
+def meanAbsoluteError(yTrue: DataType, yPred: DataType):
+    return skMetrics.mean_absolute_error(yTrue.toArray(), yPred.toArray())
 
-def meanSquaredError(y_true: DataType, y_pred: DataType):
-    return skMetrics.mean_squared_error(y_true.toArray(), y_pred.toArray())
+def meanSquaredError(yTrue: DataType, yPred: DataType):
+    return skMetrics.mean_squared_error(yTrue.toArray(), yPred.toArray())
 
-def meanSquaredLogError(y_true: DataType, y_pred: DataType):
-    return skMetrics.mean_squared_log_error(y_true.toArray(), y_pred.toArray())
+def meanSquaredLogError(yTrue: DataType, yPred: DataType):
+    return skMetrics.mean_squared_log_error(yTrue.toArray(), yPred.toArray())
 
-def medianAbsoluteError(y_true: DataType, y_pred: DataType):
-    return skMetrics.median_absolute_error(y_true.toArray(), y_pred.toArray())
+def medianAbsoluteError(yTrue: DataType, yPred: DataType):
+    return skMetrics.median_absolute_error(yTrue.toArray(), yPred.toArray())
 
-def r2(y_true: DataType, y_pred: DataType):
-    return skMetrics.r2_score(y_true.toArray(), y_pred.toArray())
+def r2(yTrue: DataType, yPred: DataType):
+    return skMetrics.r2_score(yTrue.toArray(), yPred.toArray())
