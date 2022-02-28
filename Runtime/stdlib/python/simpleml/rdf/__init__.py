45419d50465f398c04dd2a8eb643ce2db70ca75e
Simon Gottschalk <simon-gottschalk@kabelmail.de>
2022-02-28 18:24:26 +0100

    feat: endpoint fallback (#226)

--- Runtime/stdlib/python/simpleml/rdf/__init__.py
+++ Runtime/stdlib/python/simpleml/rdf/__init__.py
@@ -1,6 +1,8 @@
 import simpleml.util.global_configurations as global_config
 
-if global_config.local_rdf:
-    from ._sparql_connector_local import get_graph, load_query, run_query
-else:
+from ._sparql_connector import endpoint_is_running
+
+if not global_config.local_rdf and endpoint_is_running():
     from ._sparql_connector import load_query, run_query
+else:
+    from ._sparql_connector_local import get_graph, load_query, run_query
