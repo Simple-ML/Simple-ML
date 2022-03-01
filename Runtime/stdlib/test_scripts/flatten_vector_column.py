afb7f616d5878b09cd89afdfbad46d89d19e8069
Fakhar Abbas <fakhar_s124@yahoo.com>
2022-03-01 01:46:09 +0100

    feat: dataset_functions (#227)
    * addAtribute function
    
    * addAtribute function correction
    
    * Added category to vector functionality
    
    * Added functions for extracting information from date attributes
    
    * Added functionality to flatten any vector column after converting category to vector
    Also deleted file for category to vector
    
    * Adding functionality for converting geometry column into embeddings
    
    * Functionality for normalizing whole dataset
    
    * Functionality for scaling whole dataset
    
    * Fixed errors mentioned by linter
    
    * Fixed errors for list mentioned by linter
    
    * style: apply automatic fixes of linters
    
    Co-authored-by: Fakhar Abbas <abbas@l3s.de>
    Co-authored-by: fakharabbas-3002 <fakharabbas-3002@users.noreply.github.com>

---
+++ Runtime/stdlib/test_scripts/flatten_vector_column.py
@@ -0,0 +1,18 @@
+# Imports ----------------------------------------------------------------------
+from simpleml.dataset import loadDataset
+
+# Workflow steps ---------------------------------------------------------------
+
+
+def exampleWorkflow():
+    dataset = loadDataset("SpeedAverages")
+
+    dataset = dataset.categoryToVector("season")
+    dataset = dataset.flattenData()
+
+    print(dataset.data.columns.values.tolist())
+    print(dataset.data)
+
+
+if __name__ == "__main__":
+    exampleWorkflow()
---
+++ Runtime/stdlib/test_scripts/geometry.py
@@ -0,0 +1,15 @@
+# Imports ----------------------------------------------------------------------
+from simpleml.dataset import loadDataset
+
+# Workflow steps ---------------------------------------------------------------
+
+
+def exampleWorkflow():
+    dataset = loadDataset("SpeedAverages")
+
+    dataset = dataset.addGeometryEmbeddings("geometry")
+    print(dataset.data)
+
+
+if __name__ == "__main__":
+    exampleWorkflow()
