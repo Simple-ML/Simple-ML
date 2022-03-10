15239bd94c2012901bd3cc2a498021d6d604bc80
sgottsch <simon-gottschalk@kabelmail.de>
2022-03-10 03:05:52 +0100

    provide examples of wine and speedAverages workflows (DSL and Py) and fix bugs

--- Runtime/stdlib/python/simpleml/dataset/__init__.py
+++ Runtime/stdlib/python/simpleml/dataset/__init__.py
@@ -1,3 +1,5 @@
 from ._dataset import Dataset, loadDataset, readDataSetFromCSV, joinTwoDatasets
 from ._stats import getStatistics
 from ._instance import Instance
+from ._scale import StandardScaler
+from ._normalize import StandardNormalizer
