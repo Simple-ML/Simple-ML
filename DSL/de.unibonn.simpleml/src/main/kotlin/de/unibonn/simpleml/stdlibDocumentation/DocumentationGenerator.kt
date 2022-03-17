272a29c46a9fcd9b42a1197220d674648e3176d3
Lars Reimann <mail@larsreimann.com>
2022-03-17 13:16:23 +0100

    feat: create table of contents and docs for annotations

--- DSL/de.unibonn.simpleml/src/main/kotlin/de/unibonn/simpleml/stdlibDocumentation/DocumentationGenerator.kt
+++ DSL/de.unibonn.simpleml/src/main/kotlin/de/unibonn/simpleml/stdlibDocumentation/DocumentationGenerator.kt
@@ -1,7 +1,15 @@
 package de.unibonn.simpleml.stdlibDocumentation
 
 import de.unibonn.simpleml.emf.containingCompilationUnitOrNull
+import de.unibonn.simpleml.emf.parametersOrEmpty
 import de.unibonn.simpleml.scoping.allGlobalDeclarations
+import de.unibonn.simpleml.simpleML.SmlAnnotation
+import de.unibonn.simpleml.simpleML.SmlClass
+import de.unibonn.simpleml.simpleML.SmlEnum
+import de.unibonn.simpleml.simpleML.SmlFunction
+import de.unibonn.simpleml.simpleML.SmlParameter
+import de.unibonn.simpleml.stdlibAccess.descriptionOrNull
+import de.unibonn.simpleml.stdlibAccess.validTargets
 import org.eclipse.emf.ecore.EObject
 import java.nio.file.Path
 import kotlin.io.path.createDirectories
@@ -32,7 +40,7 @@ fun EObject.generateDocumentation(outputDirectory: Path) {
 }
 
 private fun createReadme(outputDirectory: Path, packagesToDeclarations: Map<String, List<EObject>>) {
-    val packageDescriptions = packagesToDeclarations.keys
+    val packagesDocumentation = packagesToDeclarations.keys
         .sorted()
         .joinToString(separator = "\n") { packageName ->
             "* [$packageName](./${packageName.replace(".", "_")}.md)"
@@ -42,9 +50,10 @@ private fun createReadme(outputDirectory: Path, packagesToDeclarations: Map<Stri
         """
             |# Simple-ML API Documentation
             |
-            |$packageDescriptions
+            |$packagesDocumentation
             |
             |$autogenWarning
+            |
         """.trimMargin()
     )
 }
@@ -56,11 +65,99 @@ private fun createPackageFiles(outputDirectory: Path, packagesToDeclarations: Ma
 }
 
 private fun createPackageFile(outputDirectory: Path, packageName: String, globalDeclarations: List<EObject>) {
-    outputDirectory.resolve("${packageName.replace(".", "_")}.md").writeText(
-        """
-            |# Package `$packageName`
-            |
-            |$autogenWarning
-        """.trimMargin()
-    )
+    outputDirectory
+        .resolve("${packageName.replace(".", "_")}.md")
+        .writeText(createPackageDocumentation(packageName, globalDeclarations))
+}
+
+private fun createPackageDocumentation(
+    packageName: String,
+    globalDeclarations: List<EObject>
+) = buildString {
+    val classes = globalDeclarations.filterIsInstance<SmlClass>().sortedBy { it.name }
+    val globalFunctions = globalDeclarations.filterIsInstance<SmlFunction>().sortedBy { it.name }
+    val enums = globalDeclarations.filterIsInstance<SmlEnum>().sortedBy { it.name }
+    val annotations = globalDeclarations.filterIsInstance<SmlAnnotation>().sortedBy { it.name }
+
+    appendLine("# Package `$packageName`")
+
+    // Table of contents
+    if (annotations.isNotEmpty() || classes.isNotEmpty() || enums.isNotEmpty() || globalFunctions.isNotEmpty()) {
+        appendLine("\n## Table of Contents\n")
+
+        classes.forEach {
+            appendLine("* [Class `${it.name}`](#class-${it.name})")
+        }
+        globalFunctions.forEach {
+            appendLine("* [Global function `${it.name}`](#global-function-${it.name})")
+        }
+        enums.forEach {
+            appendLine("* [Enum `${it.name}`](#enum-${it.name})")
+        }
+        annotations.forEach {
+            appendLine("* [Annotation `${it.name}`](#annotation-${it.name})")
+        }
+    }
+
+    // Classes
+    classes.forEach {
+        appendLine(createClassDocumentation(it, nestingLevel = 2))
+    }
+
+    // Global functions
+    globalFunctions.forEach {
+        appendLine(createFunctionDocumentation(it, nestingLevel = 2))
+    }
+
+    // Enums
+    enums.forEach {
+        appendLine(createEnumDocumentation(it, nestingLevel = 2))
+    }
+
+    // Annotations
+    annotations.forEach {
+        appendLine(createAnnotationDocumentation(it))
+    }
+
+    appendLine("\n$autogenWarning")
+}
+
+private fun createAnnotationDocumentation(annotation: SmlAnnotation) = buildString {
+    appendLine("## Annotation `${annotation.name}`")
+
+    // Description
+    if (annotation.descriptionOrNull() != null) {
+        appendLine(annotation.descriptionOrNull())
+    } else {
+        appendLine("_No description available._")
+    }
+
+    // Parameters
+    append(createParametersDocumentation(annotation.parametersOrEmpty()))
+
+    // Targets
+    appendLine("\n**Valid targets:**")
+    val validTargets = annotation
+        .validTargets()
+        .sortedBy { it.name }
+        .joinToString(separator = "\n") {
+            "* ${it.name}"
+        }
+    append(validTargets)
+}
+
+private fun createClassDocumentation(`class`: SmlClass, nestingLevel: Int): String {
+    return ""
+}
+
+private fun createEnumDocumentation(enum: SmlEnum, nestingLevel: Int): String {
+    return ""
+}
+
+private fun createFunctionDocumentation(function: SmlFunction, nestingLevel: Int): String {
+    return ""
+}
+
+private fun createParametersDocumentation(parameters: List<SmlParameter>) = buildString {
+    appendLine("\n**Parameters:**")
 }
