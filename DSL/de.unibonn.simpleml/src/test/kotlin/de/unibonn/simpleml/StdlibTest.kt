27725c2c3b951cd79b9f4a98335ea1c0004ff132
Lars Reimann <mail@larsreimann.com>
2022-04-01 11:56:24 +0200

    test: fix failing stdlib tests

--- DSL/de.unibonn.simpleml/src/test/kotlin/de/unibonn/simpleml/StdlibTest.kt
+++ DSL/de.unibonn.simpleml/src/test/kotlin/de/unibonn/simpleml/StdlibTest.kt
@@ -6,16 +6,15 @@ import de.unibonn.simpleml.stdlibAccess.listStdlibFiles
 import de.unibonn.simpleml.testing.ParseHelper
 import de.unibonn.simpleml.testing.SimpleMLInjectorProvider
 import de.unibonn.simpleml.testing.assertions.shouldHaveNoErrorsOrWarnings
+import io.kotest.matchers.nulls.shouldNotBeNull
 import org.eclipse.xtext.testing.InjectWith
 import org.eclipse.xtext.testing.extensions.InjectionExtension
 import org.eclipse.xtext.testing.validation.ValidationTestHelper
-import org.eclipse.xtext.validation.Issue
 import org.junit.jupiter.api.DynamicNode
 import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.TestFactory
 import org.junit.jupiter.api.extension.ExtendWith
 import java.nio.file.Files
-import java.nio.file.Path
 import java.util.stream.Stream
 
 @ExtendWith(InjectionExtension::class)
@@ -30,20 +29,22 @@ class StdlibTest {
 
     @TestFactory
     fun `should not have syntax or semantic errors`(): Stream<out DynamicNode> {
+        val stdlibFiles = listStdlibFiles().map { it.first.toString() }.toList()
+
         return listStdlibFiles()
             .map { (filePath, _) ->
+
+                // We must do this here and not in the callback of the dynamicTest so the JAR file system is still open
                 val program = Files.readString(filePath)
+                val parsingResult = parseHelper.parseProgramText(program, context = stdlibFiles, loadStdlib = false)
+
                 DynamicTest.dynamicTest(filePath.toString(), filePath.toUri()) {
-                    actualIssues(filePath, program).shouldHaveNoErrorsOrWarnings()
+                    parsingResult.shouldNotBeNull()
+                    parsingResult.eResource().eAdapters().add(OriginalFilePath(filePath.toString()))
+                    validationHelper.validate(parsingResult).shouldHaveNoErrorsOrWarnings()
                 }
             }
             .toList()
             .stream()
-    }
-
-    private fun actualIssues(filePath: Path, program: String): List<Issue> {
-        val parsingResult = parseHelper.parseProgramText(program) ?: return emptyList()
-        parsingResult.eResource().eAdapters().add(OriginalFilePath(filePath.toString()))
-        return validationHelper.validate(parsingResult)
     }
 }
