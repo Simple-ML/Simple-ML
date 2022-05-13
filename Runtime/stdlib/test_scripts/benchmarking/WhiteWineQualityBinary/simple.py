f2d886d1b1c5530a07da91af170edaff7c5ff2b5
Evgeniy <evgeniy.eg1@yandex.ru>
2022-05-13 12:06:15 +0200

    feat: benchmarking (#300)
    Create benchmarking code and tables. Create dsl code generation, python code generation that recreats benchmarking results.

---
+++ Runtime/stdlib/test_scripts/benchmarking/WhiteWineQualityBinary/simple.py
@@ -0,0 +1,24 @@
+# Imports ----------------------------------------------------------------------
+from simpleml.dataset import loadDataset
+from simpleml.metrics import meanAbsoluteError
+from simpleml.model.supervised.regression import LinearRegression
+
+
+# Workflow steps ---------------------------------------------------------------
+def exampleWorkflow():
+    dataset = loadDataset("WhiteWineQualityBinary")
+
+    dataset = dataset.dropAllMissingValues()
+    dataset = dataset.transformDatatypes()
+
+    dataset = dataset.setTargetAttribute("quality")
+
+    X_train, X_test, y_train, y_test = dataset.splitIntoTrainAndTestAndLabels(0.8)
+    lr = LinearRegression().fit(X_train, y_train)
+    y_pred = lr.predict(X_test)
+
+    print("MAE:", meanAbsoluteError(y_test, y_pred))
+
+
+if __name__ == "__main__":
+    exampleWorkflow()
